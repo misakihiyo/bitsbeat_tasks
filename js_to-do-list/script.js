b21ca747795c49d25ce611d11028af023f4f2e09
misakihiyo <eva.shre@gmail.com>
2019-06-26 17:43:49 +0545

    delete edit add to list

--- js_to-do-list/script.js
+++ js_to-do-list/script.js
@@ -1,51 +1,156 @@
 var count = localStorage.length;
+
 function displayList(){
-        
+var count = localStorage.length;
   for (i=0; i<count ; i++){
     var idname = "data" + i;
     var newitem = localStorage.getItem(idname);
-    var newt= document.createElement("li");
-    var toappend = document.createTextNode(newitem);
-
-    
-    newt.appendChild(toappend);
-    newt.setAttribute("id", idname);
-    document.getElementById("listname").appendChild(newt);
-    
-    var deletebutton = document.createElement("button");    
-    var txt = document.createTextNode("button");   
-    deletebutton.className= "delbutton";    
-    deletebutton.appendChild(txt);    
-    newt.appendChild(deletebutton); 
+    puttingonlist(idname,newitem);
+       
   } 
   
-  let del = document.getElementsByClassName("delbutton");
-  console.log(del.length);
+  deletefunction();
+  clickfucntion();
+  editfunction();
   
 }
-var del= document.getElementsByTagName("li");
-console.log(del.length);
+
+
+function puttingonlist(idname,newitem){
+  var newt= document.createElement("li");
+  var toappend = document.createTextNode(newitem);
+  
+      
+  newt.appendChild(toappend);
+  newt.setAttribute("id", idname);
+  document.getElementById("listname").appendChild(newt);
+
+  var checkbox = document.createElement("span");  
+  checkbox.className="far";
+  newt.append(checkbox);
+
+  var editbutton = document.createElement("button");
+  var txtbut = document.createTextNode("Edit");
+  editbutton.className="editbutton";
+  editbutton.appendChild(txtbut);
+  newt.appendChild(editbutton);
+
+  
+  var deletebutton = document.createElement("button");    
+  var txt = document.createTextNode("Delete");   
+  deletebutton.className= "delbutton";    
+  deletebutton.appendChild(txt);    
+  newt.appendChild(deletebutton);
+}
+  
+function clickfucntion(){
+var li = document.getElementsByTagName("li");
+
+
+  for (i=0; i<li.length; i++){
+    let checkbox = document.getElementsByClassName("far");
+    let checkb = checkbox[i];
+    var count = i;
+    li[i].onclick = function(){
+      if (this.style.color == "white"){      
+        this.style.color = "black";
+        this.style.setProperty("text-decoration","none");
+        checkb.className ="far";    
+        this.style.setProperty("background-color","white");
+        
+      }
+
+      else{
+      this.style.color="white";
+      this.style.setProperty("text-decoration","line-through");
+      checkb.className="far fa-check-circle";
+      checkb.style.setProperty("margin-left","2rem");
+      checkb.style.setProperty("color","red");
+      checkb.style.setProperty("font-size","1.5rem");
+      this.style.setProperty("background-color","rgb(121, 156, 233)");
+
+      }
+    }
+  }
+} 
+
 
 function addtolist(){
   var idname = "data" + count;
   var newitem = document.getElementById("inputlist").value;
   localStorage.setItem(idname, newitem );
-  var newt= document.createElement("li");    
-  var toappend = document.createTextNode(newitem);
-  newt.appendChild(toappend);
-  newt.setAttribute("id", idname);
-  document.getElementById("listname").appendChild(newt);
-  
-  var deletebutton = document.createElement("button");    
-  var txt = document.createTextNode("button");   
-  deletebutton.className= "delbutton";    
-  deletebutton.appendChild(txt);    
-  newt.appendChild(deletebutton); 
+  puttingonlist(idname,newitem);
 
   count +=1;
-    
+  deletefunction(); 
+  clickfucntion();  
+  editfunction();
 }
 
+function deletefunction(){
+  var delbut = document.getElementsByClassName("delbutton");
+   
+  for (i = 0; i < delbut.length; i++) {
+    
+    delbut[i].onclick = function() {
+      var div = this.parentElement;
+      var key = div.id;
+      localStorage.removeItem(key);
+      var ul = div.parentElement;
+      ul.removeChild(div);   
+            
+    } 
+  }
+}
+
+function editfunction(){
+  let editbut = document.getElementsByClassName("editbutton");
+
+  for (i = 0; i < editbut.length; i++) {
+    
+    editbut[i].onclick = function(event) {
+      event.stopPropagation();
+      var modal = document.getElementById("myModal");
+      var editbutton = document.getElementById("edit");
+
+      modal.style.display ="block";
+      var val=document.getElementById("modaltext").value;
+      var div = this.parentElement;
+      var key = div.id;
+      
+      editbutton.onclick = function(){
+        modal.style.display="none";
+        console.log(val);
+        
+        var textnode = document.createTextNode(val);
+        
+        div.replaceChild(textnode,div.childNodes[0]);
+        
+        localStorage.setItem(key,val);
+      }
+      
+            
+    }; 
+  }
+  
+}
+
+
+
+
+
+
+// function replacep(i){
+//   let li = document.getElementsByTagName("li");
+//   for (var j=i; j<li.length; j++){
+//     let m = j+1;
+//     let idname = "data" +j;
+//     let idname2 = "data" + m;
+//     let itemval = localStorage.getItem(idname2);
+//     console.log(itemval);
+//     localStorage.setItem(idname, itemval);
+//   }
+// }
 
 
 
