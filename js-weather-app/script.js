1d454ec520e56a4697dc6d19e0f10ca5b70b3ec8
misakihiyo <eva.shre@gmail.com>
2019-07-07 10:49:27 +0545

    weather app complete

--- js-weather-app/script.js
+++ js-weather-app/script.js
@@ -7,25 +7,25 @@ let icon = document.getElementById("icon");
 let temperature = document.getElementById("temp");
 let humidity = document.getElementById("humidity");
 
-searchButton.addEventListener("click", findWeatherDetails);
-searchInput.addEventListener("keyup", enterPressed);
+searchButton.addEventListener("click", weatherDetails);
+searchInput.addEventListener("keyup", enterPress);
 
-function enterPressed(event) {
+function enterPress(event) {
   if (event.key === "Enter") {
-    findWeatherDetails();
+    weatherDetails();
   }
 }
 
-function findWeatherDetails() {
+function weatherDetails() {
   if (searchInput.value === "") {
   
   }else {
     let searchLink = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid="+appKey;
-   httpRequestAsync(searchLink, theResponse);
+   httpRequestAsync(searchLink, response);
   }
  }
 
-function theResponse(response) {
+function response(response) {
   let jsonObject = JSON.parse(response);
   cityName.innerHTML = jsonObject.name;
   icon.src = "http://openweathermap.org/img/w/" + jsonObject.weather[0].icon + ".png";
@@ -35,12 +35,12 @@ function theResponse(response) {
 
 function httpRequestAsync(url, callback)
 {
-  console.log("hello");
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => { 
-        if (httpRequest.readyState == 4 && httpRequest.status == 200)
-            callback(httpRequest.responseText);
-    }
-    httpRequest.open("GET", url, true); // true for asynchronous 
-    httpRequest.send();
+  
+  var httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = () => { 
+      if (httpRequest.readyState == 4 && httpRequest.status == 200)
+          callback(httpRequest.responseText);
+  }
+  httpRequest.open("GET", url, true); // true for asynchronous 
+  httpRequest.send();
 }
